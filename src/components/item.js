3037d09b7c8f9ce9fa203c500e5916ed0225dd4c
Daniel-dev-s <daniilshevelev10@gmail.com>
2020-12-16 18:46:05 +0300

    add edit and delete functions, fix errors

--- src/components/item.js
+++ src/components/item.js
@@ -2,31 +2,30 @@ import React from 'react'
 
 export class Item extends React.Component {
 
-	constructor(props) {
-	    super(props);
-	    this.state = { 
-	    	isMusicPlaying: false 
-	    };
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            edit: false
+        };
+    }
 
-	handleChangeCheckbox(event){
-		let title = event.target.nextSibling;
-
-		event.target.checked ? 
-		title.classList.add('item-done')
-		:
-		title.classList.remove('item-done');
-	}
-
+    handleChangeCheckbox(event) {
+        let title = event.target.nextSibling;
+        event.target.checked ?
+            title.classList.add('item-done')
+            :
+            title.classList.remove('item-done');
+    }
 
     render() {
-	    return (
-	      <div className="todo-item">
-	      <div>
-	        <input type="checkbox" onChange={this.handleChangeCheckbox.bind(this)} />
-	        <span> { this.props.title } </span>
-	      </div>
-	      </div>
-	    );
+        return (
+            <div className="todo-item">
+                <div>
+                    <input type="checkbox" onChange={this.handleChangeCheckbox.bind(this)}/>
+                    <span> {this.props.title} </span><br/>
+                    <span> {this.props.create_date} </span>
+                </div>
+            </div>
+        );
     }
 }
